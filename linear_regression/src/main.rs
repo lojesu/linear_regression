e14ca6266881ce7b65e0ec4087b51eb524590edd
lojesu <lojesu@student.42.fr>
2022-04-25 16:45:26 +0200

    graph png

--- linear_regression/src/main.rs
+++ linear_regression/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use std::process;
+use plotters::prelude::*;
 
 fn is_number(s: &String) -> bool {
     for (i, c) in s.chars().enumerate() {
@@ -44,17 +45,27 @@ fn main() {
         }
     };
 
-    let tetas: Vec<&str> = content_file.trim().split(',').collect();
+    let values: Vec<&str> = content_file.trim().split(',').collect();
     let mut teta_zero: f32 = 0.0;
     let mut teta_one: f32 = 0.0;
+    let mut max_x: f32 = 0.0;
+    let mut max_y: f32 = 0.0;
     let km_f32: f32;
-    if tetas.len() == 2 {
-        match tetas[0].parse::<f32>() {
+    if values.len() == 4 {
+        match values[0].parse::<f32>() {
             Ok(n) => teta_zero = n,
             Err(_) => (),
         }
-        match tetas[1].parse::<f32>() {
+        match values[1].parse::<f32>() {
             Ok(n) => teta_one = n,
+            Err(_) => (),
+        }
+        match values[2].parse::<f32>() {
+            Ok(n) => max_x = n,
+            Err(_) => (),
+        }
+        match values[3].parse::<f32>() {
+            Ok(n) => max_y = n,
             Err(_) => (),
         }
     }
@@ -69,4 +80,52 @@ fn main() {
        _ => (),
     }
     println!("estimated price is {}", result);
+    let root_area = BitMapBackend::new("graph.png", (900, 900))
+    .into_drawing_area();
+    root_area.fill(&WHITE).unwrap();
+
+    let mut ctx = ChartBuilder::on(&root_area)
+    .set_label_area_size(LabelAreaPosition::Left, 40)
+    .set_label_area_size(LabelAreaPosition::Bottom, 40)
+    .caption("Linear Regression", ("sans-serif", 40))
+    .build_cartesian_2d(0..(max_x + 0.15 * max_x) as i32, 0..(max_y + 0.15 * max_y) as i32)
+    .unwrap();
+
+    ctx.configure_mesh().draw().unwrap();
+
+    ctx.draw_series(
+    LineSeries::new((0..=(max_x + 0.15 * max_x) as i32).map(|x| (x, (teta_zero + teta_one * (x as f32)) as i32)), &GREEN)
+    ).unwrap();
+
+    let mut kms = Vec::new();
+    let mut prices = Vec::new();
+
+    let content_file = match fs::read_to_string("../data.csv") {
+        Ok(content_file) => content_file,
+        Err(_) => {
+            println!("something went wrong reading the file for draw the graph");
+            process::exit(1);
+        }
+    };
+    for line in content_file.trim().split('\n') {
+        let line_split: Vec<&str> = line.split(',').collect(); 
+        if line_split.len() == 2 {
+            match line_split[0].parse::<f32>() {
+                Ok(n) => kms.push(n),
+                Err(_) => (),
+            }
+            match line_split[1].parse::<f32>() {
+                Ok(n) => prices.push(n),
+                Err(_) => (),
+            }
+        }
+    }
+    let mut data = Vec::new();
+    let mut i: usize = 0;
+    while i < kms.len() {
+        data.push((kms[i] as i32, prices[i] as i32));
+        i += 1;
+    }
+    ctx.draw_series(data.iter().map(|point| Circle::new(*point, 2, &RED)))
+        .unwrap();
 }
--- training/src/main.rs
+++ training/src/main.rs
@@ -2,6 +2,7 @@ use std::{env, process};
 use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
+use std::cmp::max;
 
 mod parse;
 mod calcul;
@@ -24,13 +25,23 @@ fn main() -> std::io::Result<()> {
     for line in content_file.trim().split('\n') {
         let line_split: Vec<&str> = line.split(',').collect(); 
         if line_split.len() == 2 {
-            match line_split[0].parse::<f32>() {
-                Ok(n) => kms.push(n),
-                Err(_) => (),
+            if line_split[0].len() > 0 {
+                match line_split[0].parse::<f32>() {
+                    Ok(n) => kms.push(n),
+                    Err(_) => (),
+                }
+            } else {
+                println!("Something went wrong when reading the file");
+                process::exit(1);
             }
-            match line_split[1].parse::<f32>() {
-                Ok(n) => prices.push(n),
-                Err(_) => (),
+            if line_split[1].len() > 0 {
+                match line_split[1].parse::<f32>() {
+                    Ok(n) => prices.push(n),
+                    Err(_) => (),
+                }
+            } else {
+                println!("Something went wrong when reading the file");
+                process::exit(1);
             }
         }
     }
@@ -38,8 +49,10 @@ fn main() -> std::io::Result<()> {
     
     let teta_one = calcul::teta_one(&kms, &prices);
     let teta_zero = calcul::teta_zero(&kms, &prices, teta_one);
+    let max_x = kms.iter().copied().fold(f32::NAN, f32::max);
+    let max_y = prices.iter().copied().fold(f32::NAN, f32::max);
 
     let mut f = File::create("../my_data.csv")?;
-    f.write(format!("{},{}\n", teta_zero.to_string(), teta_one.to_string()).as_bytes())?;
+    f.write(format!("{},{},{},{}\n", teta_zero.to_string(), teta_one.to_string(), max_x.to_string(), max_y.to_string()).as_bytes())?;
     Ok(())
 }
